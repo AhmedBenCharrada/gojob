95dcc24e1438eef38893caf9c1bf2aa99bbccf97
ahmad <ahmed.b.charrada@gmail.com>
2022-12-03 13:13:43 +0100

    Add retry function.

---
+++ retry.go
@@ -0,0 +1,82 @@
+package gojob
+
+import (
+	"context"
+	"fmt"
+	"math"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+const (
+	DefaultMaxRetries   = 5
+	DefaultInitialDelay = 100 * time.Millisecond
+	DefaultMaxDelay     = 100 * time.Millisecond
+)
+
+func Run[T any](ctx context.Context, fn func() (T, error), maxTries int, initialDelay, maxDelay time.Duration, exitErrs []error) (T, error) {
+	maxTries = orElse(maxTries > 0, maxTries, DefaultMaxRetries)
+	initialDelay = orElse(initialDelay > 0, initialDelay, DefaultInitialDelay)
+	maxDelay = orElse(maxDelay > 0, maxDelay, DefaultMaxDelay)
+
+	attempts := 0
+	for {
+		res, err := fn()
+		if err == nil || include(exitErrs, err) {
+			return res, err
+		}
+
+		attempts++
+		if attempts == maxTries {
+			return res, fmt.Errorf("max attempt exceeded: %w", err)
+		}
+
+		ticker := time.NewTimer(nextBackOff(attempts, initialDelay, maxDelay))
+		select {
+		case <-ticker.C:
+			continue
+		case <-ctx.Done():
+			return res, fmt.Errorf("context timeout: %w", err)
+		}
+	}
+}
+
+func nextBackOff(attempt int, initialDelay, maxDelay time.Duration) time.Duration {
+	max := float64(maxDelay)
+	min := float64(initialDelay)
+
+	randPool := sync.Pool{
+		New: func() any {
+			return rand.Float64()
+		},
+	}
+
+	d := min * math.Pow(2, float64(attempt))
+	d = orElse(d < max, d, max)
+
+	rand := randPool.Get().(float64)
+	return time.Duration(rand*(d-min) + min)
+}
+
+func include(errs []error, err error) bool {
+	if len(errs) == 0 {
+		return false
+	}
+
+	for _, e := range errs {
+		if e.Error() == err.Error() {
+			return true
+		}
+	}
+
+	return false
+}
+
+func orElse[T comparable](cond bool, val, other T) T {
+	if cond {
+		return val
+	}
+
+	return other
+}
---
+++ retry_test.go
@@ -0,0 +1,70 @@
+package gojob_test
+
+import (
+	"context"
+	"fmt"
+	"gojob"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestRun(t *testing.T) {
+	t.Run("successfully", func(t *testing.T) {
+		fn := func() (string, error) {
+			return "done", nil
+		}
+
+		res, err := gojob.Run(context.TODO(), fn, 0, 0, 0, nil)
+		assert.NoError(t, err)
+		assert.Equal(t, "done", res)
+	})
+
+	t.Run("fail with an exit error", func(t *testing.T) {
+		fn := func() (string, error) {
+			return "", fmt.Errorf("breaking error")
+		}
+
+		res, err := gojob.Run(context.TODO(), fn, 0, 0, 0, []error{
+			fmt.Errorf("breaking error"),
+		})
+		assert.Error(t, err)
+		assert.Equal(t, fmt.Errorf("breaking error"), err)
+		assert.Equal(t, "", res)
+	})
+
+	t.Run("fail after max attempts", func(t *testing.T) {
+		tries := 0
+		fn := func() (string, error) {
+			tries++
+			return "", fmt.Errorf("error")
+		}
+
+		res, err := gojob.Run(context.TODO(), fn, 3, 0, 0, []error{
+			fmt.Errorf("breaking error"),
+		})
+		assert.Error(t, err)
+		assert.True(t, strings.Contains(err.Error(), "max attempt exceeded"))
+
+		assert.Equal(t, "", res)
+
+		assert.Equal(t, 3, tries)
+	})
+
+	t.Run("fail after context timeout", func(t *testing.T) {
+		fn := func() (string, error) {
+			return "", fmt.Errorf("error")
+		}
+
+		ctx, cancel := context.WithTimeout(context.TODO(), 500*time.Millisecond)
+		defer cancel()
+
+		res, err := gojob.Run(ctx, fn, 10, time.Second, 0, nil)
+		assert.Error(t, err)
+		assert.True(t, strings.Contains(err.Error(), "context timeout"))
+
+		assert.Equal(t, "", res)
+	})
+}
