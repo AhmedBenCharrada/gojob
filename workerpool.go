38c32e7a57be89fb1ae03201109478fbe1fb1f8c
ahmad <ahmed.b.charrada@gmail.com>
2022-12-03 09:40:28 +0100

    Add worker pool.

---
+++ workerpool.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"context"
+	"fmt"
+)
+
+type worker struct {
+	id   int
+	ctx  context.Context
+	stop context.CancelFunc
+}
+
+type Job func(context.Context)
+
+type Pool struct {
+	ctx  context.Context
+	stop context.CancelFunc
+
+	jobChannel chan Job
+
+	maxWorkers  int
+	workerCount int
+	workers     []worker
+
+	started bool
+}
+
+func New(ctx context.Context, maxWorkers int, buffer int) *Pool {
+	poolCtx, stop := context.WithCancel(ctx)
+	if maxWorkers < 1 {
+		maxWorkers = 1
+	}
+
+	return &Pool{
+		ctx:         poolCtx,
+		stop:        stop,
+		jobChannel:  make(chan Job, buffer),
+		maxWorkers:  maxWorkers,
+		workerCount: 0,
+		workers:     make([]worker, maxWorkers),
+	}
+}
+
+func (p *Pool) Start() error {
+	if p.started {
+		return nil
+	}
+
+	p.started = true
+	if p.workerCount < 1 {
+		return p.AddWorkers(1)
+	}
+
+	return p.AddWorkers(p.workerCount)
+}
+
+func (p *Pool) Stop() {
+	p.stop()
+}
+
+func (p *Pool) Push(job Job) error {
+	select {
+	case p.jobChannel <- job:
+		return nil
+	default:
+		return fmt.Errorf("work load exceed the limit")
+	}
+}
+
+func (p *Pool) AddWorkers(count int) error {
+	if p.workerCount+count > p.maxWorkers {
+		return fmt.Errorf("exceeded max allowed workers, remaining: %v", p.maxWorkers-p.workerCount)
+	}
+
+	p.workerCount += count
+	if !p.started {
+		return nil
+	}
+
+	for i := 0; i < count; i++ {
+		ctx, stop := context.WithCancel(p.ctx)
+
+		w := worker{
+			id:   p.workerCount,
+			ctx:  ctx,
+			stop: stop,
+		}
+
+		p.workers = append(p.workers, w)
+		go w.start(ctx, p.jobChannel)
+	}
+
+	return nil
+}
+
+func (p *Pool) DeleteWorkers(count int) error {
+	if len(p.workers)-count < 1 {
+		return fmt.Errorf("can't have a worker pool without any worker")
+	}
+
+	for i := 0; i < count; i++ {
+		w := p.workers[len(p.workers)-1]
+		w.stop()
+		p.workers = p.workers[:len(p.workers)-1]
+	}
+
+	p.workerCount -= count
+	return nil
+}
+
+func (w *worker) start(ctx context.Context, ch <-chan Job) {
+	for {
+		select {
+		case <-ctx.Done():
+			return
+		case job := <-ch:
+			job(ctx)
+		}
+	}
+}
---
+++ workerpool_test.go
@@ -0,0 +1,75 @@
+package main
+
+import (
+	"context"
+	"math/rand"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestWorkerPool(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.TODO(), 21*time.Second)
+	defer cancel()
+
+	pool := New(ctx, 5, 100)
+
+	err := pool.Start()
+	assert.NoError(t, err)
+
+	err = pool.AddWorkers(4)
+	assert.NoError(t, err)
+
+	jobCount := 10
+	ch := make(chan bool, jobCount)
+	wg := &sync.WaitGroup{}
+	wg.Add(jobCount)
+
+	jobs := getTestJobs(wg, ch, jobCount, 1000)
+
+	go func() {
+		wg.Wait()
+		close(ch)
+	}()
+
+	for _, job := range jobs {
+		err := pool.Push(job)
+		assert.NoError(t, err)
+	}
+
+loop:
+	for {
+		select {
+		case <-ctx.Done():
+			t.Fail()
+			break loop
+		case r := <-ch:
+			if r {
+				jobCount--
+				continue
+			}
+
+			break loop
+		}
+	}
+
+	assert.Equal(t, 0, jobCount)
+}
+
+func getTestJobs(wg *sync.WaitGroup, ch chan bool, count int, delayInMs int) []Job {
+	rand := rand.New(rand.NewSource(time.Now().UnixNano()))
+
+	jobs := make([]Job, 0, count)
+
+	for i := 0; i < count; i++ {
+		jobs = append(jobs, func(ctx context.Context) {
+			defer wg.Done()
+			time.Sleep(time.Duration(delayInMs+rand.Intn(1000)) * time.Millisecond)
+			ch <- true
+		})
+	}
+
+	return jobs
+}
